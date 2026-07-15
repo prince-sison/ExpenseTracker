d49869e78d1c14b051cc7fed047c3b9a5512fadb
princesison-nlplogix <prince.sison@nlplogix.com>
2026-07-15 16:13:54 +0000

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added React and Vite setup with main entry point and index CSS.
    - Included SVG assets for React and Vite logos.
    - Configured Tailwind CSS with DaisyUI for theming.
    - Created TypeScript configuration files for app and node.
    - Set up Vite configuration for React and Tailwind CSS integration.
    
    feat: initialize backend with ASP.NET Core
    
    - Created ASP.NET Core Web API project structure.
    - Added necessary packages for OpenAPI and Entity Framework Core.
    - Implemented basic health check and weather forecast endpoints.
    - Configured logging and launch settings for development.
    - Established project references for application, infrastructure, and domain layers.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        data-theme="night"
+        className="min-h-screen bg-base-100 text-base-content"
+      >
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
