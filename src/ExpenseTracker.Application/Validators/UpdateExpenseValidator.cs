77d1c261c5f92fa7803cb3e462e10b61f3fe03fa
princesison-nlplogix <prince.sison@nlplogix.com>
2026-07-17 12:30:55 +0000

    feat: add UpdateExpenseValidator for expense update validation

---
+++ src/ExpenseTracker.Application/Validators/UpdateExpenseValidator.cs
@@ -0,0 +1,29 @@
+using ExpenseTracker.Application.DTOs.Expenses;
+using FluentValidation;
+
+namespace ExpenseTracker.Application.Validators;
+
+public class UpdateExpenseValidator : AbstractValidator<UpdateExpenseDto>
+{
+    public UpdateExpenseValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("An expense id is required.");
+
+        RuleFor(x => x.Amount)
+            .GreaterThan(0).WithMessage("Amount must be greater than zero.")
+            .LessThanOrEqualTo(999_999.99m).WithMessage("Amount exceeds the maximum allowed.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .MaximumLength(255).WithMessage("Description cannot exceed 255 characters.");
+
+        RuleFor(x => x.CategoryId)
+            .NotEmpty().WithMessage("A category is required.");
+
+        RuleFor(x => x.Date)
+            .NotEmpty().WithMessage("Date is required.")
+            .LessThanOrEqualTo(DateOnly.FromDateTime(DateTime.UtcNow))
+            .WithMessage("Expense date cannot be in the future.");
+    }
+}
