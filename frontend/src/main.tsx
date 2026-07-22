4ccd351a35f7b6ca8652fe7a46040eaa3fe51428
princesison-nlplogix <prince.sison@nlplogix.com>
2026-07-22 14:36:48 +0000

    fix: remove file extension from router import in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./api/router/AppRouter.tsx";
+import { router } from "./api/router/AppRouter";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       {/* React Query's cache provider */}
-      <RouterProvider router={router} />{" "}
+      <RouterProvider router={router} />
       {/* React Router's provider for routing */}
     </QueryClientProvider>
   </StrictMode>,
